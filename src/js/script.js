2963630bd5ad6eabaf503f2a87cc184dcd73c5d2
Julia <69167984+Julia-Kalyukh@users.noreply.github.com>
2021-12-07 00:05:36 +0300

    add tabs
     jQuery

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
-// Запуск слайдера после загрузки всего документа
+// Запуск после загрузки всего документа
 $(document).ready(function(){
+    // Слайдер
     $('.carousel__inner').slick({
         speed: 500,
         autoplay: true,
@@ -7,4 +8,24 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
     });
+
+    // Табы
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    // Ссылки
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
