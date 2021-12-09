1ea577480fe5f3152492a392f79be4d40ece26f5
Julia <69167984+Julia-Kalyukh@users.noreply.github.com>
2021-12-09 23:28:01 +0300

    add modal
    jQuery

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 // Запуск после загрузки всего документа
 $(document).ready(function(){
-    // Слайдер
+    // Slider
     $('.carousel__inner').slick({
         speed: 500,
         autoplay: true,
@@ -9,14 +9,14 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
     });
 
-    // Табы
+    // Tabs
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
     $(this)
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
-    // Ссылки
+    // Links
     function toggleSlide(item) {
         $(item).each(function(i) {
             $(this).on('click', function(e) {
@@ -28,4 +28,19 @@ $(document).ready(function(){
     }
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // Modal
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
 });
