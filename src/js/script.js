538a975553f723026dd4781b33c5e991755a1cae
Julia <69167984+Julia-Kalyukh@users.noreply.github.com>
2021-12-12 15:39:26 +0300

    Smooth scroll and pageup

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 // Запуск после загрузки всего документа
 $(document).ready(function(){
+    
     // Slider
     $('.carousel__inner').slick({
         speed: 500,
@@ -72,6 +73,8 @@ $(document).ready(function(){
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+
+    // Letters
     $('form').submit(function(e) {
         e.preventDefault();
 
@@ -89,6 +92,22 @@ $(document).ready(function(){
             $('.overlay, #thanks').fadeIn('slow');
             $('form').trigger('reset');
         });
+        return false;
+    });
+
+
+    // Smooth scroll and pageup
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
     });
 
